d1a4992ae013ca0d3e9bea8b2f14946fd722e2b6
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-08-12 18:28:30 +0900

    add prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 interface Root {
-  querySelectorAll: typeof document.querySelectorAll
+  querySelectorAll: typeof document.querySelectorAll;
 }
 
 export const apply = async (text: string, root: Root) => {
-  const frames = root.querySelectorAll('nanowire-frame');
+  const frames = root.querySelectorAll("nanowire-frame");
   const frameMap = new Map<string, Element>();
-  frames.forEach(el => {
-    if (el.id !== '') {
+  frames.forEach((el) => {
+    if (el.id !== "") {
       if (frameMap.has(el.id)) {
-        console.warn(`Some frames has a same ID: "${el.id}". It might cause invalid rendering.`);
+        console.warn(
+          `Some frames has a same ID: "${el.id}". It might cause invalid rendering.`
+        );
       }
       frameMap.set(el.id, el);
     }
-  })
+  });
   const parser = new DOMParser();
-  const newFrames = parser.parseFromString(text, 'text/html')
-    .querySelectorAll('nanowire-frame:not(nanowire-frame nanowire-frame)');
-  newFrames.forEach(el => {
-    const dist = frameMap.get(el.id)
-    if (dist != null) {
-      dist.replaceWith(el)
+  const newFrames = parser
+    .parseFromString(text, "text/html")
+    .querySelectorAll("nanowire-frame:not(nanowire-frame nanowire-frame)");
+  newFrames.forEach((el) => {
+    const dest = frameMap.get(el.id);
+    if (dest != null) {
+      dest.replaceWith(el);
     }
-  })
-}
+  });
+};
