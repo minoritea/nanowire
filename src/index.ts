7f4db7e69ed03492a3455b8794e16caf931881e6
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-08-16 10:36:12 +0900

    use querySelector to search target element

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,19 @@ import morphdom from "morphdom";
 
 interface Root {
   querySelectorAll: typeof document.querySelectorAll;
+  querySelector: typeof document.querySelector;
 }
 
 export const apply = async (root: Root, text: string) => {
-  const frames = root.querySelectorAll("nw-frame");
-  const frameMap = new Map<string, Element>();
-  frames.forEach((el) => {
-    if (el.id !== "") {
-      if (frameMap.has(el.id)) {
-        console.warn(
-          `Some frames has a same ID: "${el.id}". It might cause invalid rendering.`
-        );
-      }
-      frameMap.set(el.id, el);
-    }
-  });
   const parser = new DOMParser();
-  const newFrames = parser
+  const frames = parser
     .parseFromString(text, "text/html")
     .querySelectorAll("nw-frame:not(nw-frame nw-frame)");
-  newFrames.forEach((el) => {
-    const dest = frameMap.get(el.id);
+  frames.forEach((el) => {
+    if (el.id === "") {
+      return;
+    }
+    const dest = root.querySelector(`nw-frame#${el.id}`);
     if (dest != null) {
       morphdom(dest, el);
     }
