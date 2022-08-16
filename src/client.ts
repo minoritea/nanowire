730c4500ccf5854ddf896f64b95f674f89a4b8e4
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-08-16 22:29:53 +0900

    add nanowire/client

---
+++ src/client.ts
@@ -0,0 +1,43 @@
+import { apply } from "./";
+
+export const client = {
+  async get(url: Request | URL | string, init?: RequestInit) {
+    init = { ...init, method: "GET" };
+    const res = await fetch(url, init);
+    return apply(document, await res.text());
+  },
+
+  async post(
+    url: Request | URL | string,
+    body?: BodyInit | null,
+    init?: RequestInit
+  ) {
+    init = { ...init, method: "POST" };
+    if (body != null) {
+      init.body = body;
+    }
+    const res = await fetch(url, init);
+    return apply(document, await res.text());
+  },
+
+  async put(
+    url: Request | URL | string,
+    body?: BodyInit | null,
+    init?: RequestInit
+  ) {
+    init = { ...init, method: "PUT" };
+    if (body != null) {
+      init.body = body;
+    }
+    const res = await fetch(url, init);
+    return apply(document, await res.text());
+  },
+
+  async delete(url: Request | URL | string, init?: RequestInit) {
+    init = { ...init, method: "DELETE" };
+    const res = await fetch(url, init);
+    return apply(document, await res.text());
+  },
+};
+
+export default client;
