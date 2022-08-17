9419b660f27673010bb55966c12e71d618dfe358
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-08-17 09:17:08 +0900

    fix example

--- example/public/js/nanowire.mjs
+++ example/public/js/nanowire.mjs
@@ -472,22 +472,13 @@ var morphdom_esm_default = morphdom;
 
 // ../src/index.ts
 var apply = async (root, text) => {
-  const frames = root.querySelectorAll("nw-frame");
-  const frameMap = /* @__PURE__ */ new Map();
-  frames.forEach((el) => {
-    if (el.id !== "") {
-      if (frameMap.has(el.id)) {
-        console.warn(
-          `Some frames has a same ID: "${el.id}". It might cause invalid rendering.`
-        );
-      }
-      frameMap.set(el.id, el);
-    }
-  });
   const parser = new DOMParser();
-  const newFrames = parser.parseFromString(text, "text/html").querySelectorAll("nw-frame:not(nw-frame nw-frame)");
-  newFrames.forEach((el) => {
-    const dest = frameMap.get(el.id);
+  const frames = parser.parseFromString(text, "text/html").querySelectorAll("nw-frame:not(nw-frame nw-frame)");
+  frames.forEach((el) => {
+    if (el.id === "") {
+      return;
+    }
+    const dest = root.querySelector(`nw-frame#${el.id}`);
     if (dest != null) {
       morphdom_esm_default(dest, el);
     }
