fa3ba55274a764c29a7c84d366b3cc48b1e656dd
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-08-17 23:16:30 +0900

    fix

--- src/test/client.test.ts
+++ src/test/client.test.ts
@@ -7,7 +7,7 @@ applyMock.mockResolvedValue(Promise.resolve())
 afterEach(() => applyMock.mockClear())
 
 const fetchMock = jest.fn(() => Promise.resolve({
-  text() {return Promise.resolve('mock text')}
+  text() {return Promise.resolve("mock text")}
 } as any as Response));
 globalThis.fetch = fetchMock;
 afterEach(() => fetchMock.mockClear())
@@ -15,23 +15,23 @@ afterEach(() => fetchMock.mockClear())
 test('client.get should call "fetch" and "apply"', async () => {
   await client.get("test-url")
   expect(fetchMock).toBeCalledWith("test-url", {method: "GET"});
-  expect(applyMock).toBeCalledWith(document, 'mock text');
+  expect(applyMock).toBeCalledWith(document, "mock text");
 })
 
 test('client.post should call "fetch" and "apply"', async () => {
   await client.post("test-url")
   expect(fetchMock).toBeCalledWith("test-url", {method: "POST"});
-  expect(applyMock).toBeCalledWith(document, 'mock text');
+  expect(applyMock).toBeCalledWith(document, "mock text");
 })
 
 test('client.put should call "fetch" and "apply"', async () => {
   await client.put("test-url")
   expect(fetchMock).toBeCalledWith("test-url", {method: "PUT"});
-  expect(applyMock).toBeCalledWith(document, 'mock text');
+  expect(applyMock).toBeCalledWith(document, "mock text");
 })
 
 test('client.delete should call "fetch" and "apply"', async () => {
   await client.delete("test-url")
   expect(fetchMock).toBeCalledWith("test-url", {method: "DELETE"});
-  expect(applyMock).toBeCalledWith(document, 'mock text');
+  expect(applyMock).toBeCalledWith(document, "mock text");
 })
